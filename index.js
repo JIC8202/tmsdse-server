ac79fd5fefc34f524b80320a89cc7290a46f896b
despair <>
2018-10-31 02:14:14 -0400

    Allow cross-origin requests

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var express = require('express');
+var cors = require('cors');
 var fs = require('fs');
 
 var app = express();
+app.use(cors()); // enable cross-origin requests
 
 var data = JSON.parse(fs.readFileSync('data.json', 'utf8'));
 
