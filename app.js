25c9dcb32c76ca5e8eb384705f8f00bc9ba3dba4
despair <>
2018-11-30 07:04:22 -0500

    Load Mongo URI through .env
    Don't wait for simulation to start server

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const cors = require('cors');
 const express = require('express');
-const {MongoClient} = require('mongodb');
 
 const Simulation = require('./simulation');
 
@@ -8,9 +7,7 @@ const dataRoute = require('./routes/data');
 const linkRoute = require('./routes/link');
 const nodeRoute = require('./routes/node');
 
-const url = process.argv[2];
-
-module.exports = async function() {
+module.exports = function(db) {
     var app = express();
 
     // enable cross-origin requests
@@ -24,14 +21,11 @@ module.exports = async function() {
     app.use('/link', linkRoute);
     app.use('/node', nodeRoute);
 
-    let client = await MongoClient.connect(url, {useNewUrlParser: true});
-    console.log('Connected to MongoDB');
-    let db = client.db();
     app.locals.db = db;
 
     let simulation = new Simulation(app);
     app.locals.simulation = simulation;
-    await simulation.update();
+    simulation.update();
 
     return app;
 };
--- server.js
+++ server.js
@@ -1,16 +1,24 @@
 const fs = require('fs');
+const {MongoClient} = require('mongodb');
 const spdy = require('spdy');
+
 const loadApp = require('./app');
+require('dotenv').config();
 
 const credentials = {
     key: fs.readFileSync('sslcert/privkey.pem', 'utf8'),
     cert: fs.readFileSync('sslcert/fullchain.pem', 'utf8')
 };
 
-const port = process.env.PORT || 443;
+(async () => {
+    let client = await MongoClient.connect(process.env.MONGO_URI,
+        {useNewUrlParser: true});
+    console.log('Connected to MongoDB');
 
-loadApp().then(app => {
-    const server = spdy.createServer(credentials, app);
+    let port = process.env.PORT || 443;
+    let app = loadApp(client.db());
+
+    let server = spdy.createServer(credentials, app);
     server.listen(port);
     console.log(`Server listening on :${port}`);
-});
+})();
